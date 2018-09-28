1c69bcf61648b37fc11ed18348195771181f9093
kgunapati <kgunapati@guildmortgage.net>
2018-09-28 08:44:30 -0700

    layout

--- src/FileViewer.js
+++ src/FileViewer.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
+import cancel from "./cancel.png";
 
-const FileViewer=({ url, alt, onClick})=>(
-     <div className="card">
-        <img
-            src={url}
-            alt={alt}
-            className="image"
-            hspace="5"
-            onClick={onClick}
-          />
- </div>
-)
+const FileViewer = ({ url, alt, onClick, onDeleteImage }) => (
+  <div className="card">
+    <img src={cancel} className="delete-image" onClick={onDeleteImage}/>
+    <img src={url} alt={alt} className="image" hspace="5" onClick={onClick} />
+  </div>
+);
 
 export default FileViewer;
