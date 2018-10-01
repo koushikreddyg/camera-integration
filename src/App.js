8c78bb7d4e7c765844ee26061962eda1fd8810ad
koushikui <koushikreddygunapati@gmail.com>
2018-09-30 18:30:03 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* !this.checkForCamera() && <h1>Camera is not supported</h1> */}
-        {true && (
+        { !this.checkForCamera() && <h1>Camera is not supported</h1> }
+        {this.checkForCamera() && (
           <div>
             <ImageUpload pdfDataUrl={this.pdfDownload} />
           </div>
